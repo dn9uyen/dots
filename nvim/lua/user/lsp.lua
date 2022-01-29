0c4b34354a49c81d37a43b934e769e9bab04a3c6
dn9uyen <dannynguyen84268@gmail.com>
2022-01-28 19:13:18 -0800

    Change keybinds

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -46,8 +46,8 @@ for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
     flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
+        -- This will be the default in neovim 0.7+
+        debounce_text_changes = 150,
     }
   }
 end
