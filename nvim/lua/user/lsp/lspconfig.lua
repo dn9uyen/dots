bca046994735a39dc68701249a0fe1924e2c2db1
dn9uyen <dannynguyen84268@gmail.com>
2022-12-29 15:29:40 -0800

    Update lspconfig.lua

--- nvim/lua/user/lsp/lspconfig.lua
+++ nvim/lua/user/lsp/lspconfig.lua
@@ -82,7 +82,7 @@ local lsp_flags = {
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 
 
