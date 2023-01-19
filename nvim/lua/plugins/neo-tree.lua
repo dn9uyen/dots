88ecf22b41fc1dd8ca89927be11ae8c169b54afe
dn9uyen <dannynguyen84268@gmail.com>
2023-01-19 10:04:11 -0800

    Add files via upload

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = { 
+	"nvim-lua/plenary.nvim",
+	"kyazdani42/nvim-web-devicons",
+	"MunifTanjim/nui.nvim",
+    },
+    config = true,
+}
