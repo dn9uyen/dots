2ae4353269e88fdcacabebc6da5091b96d8cb3d5
dn9uyen <dannynguyen84268@gmail.com>
2023-10-08 09:21:55 -0700

    bug fixes

--- nvim/lua/plugins/ultimate-autopair.lua
+++ nvim/lua/plugins/ultimate-autopair.lua
@@ -4,8 +4,6 @@ return {
     branch = 'v0.6',
     config = function()
         require("ultimate-autopair").setup({
-            -- Only balance space when non-space character inserted
-            { space = { enable = false }, space2 = { enable = true } }
         })
     end,
 }
